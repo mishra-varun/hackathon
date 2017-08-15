e57b87b8a2df69deaa92e32910742dfce842c58b
varun mishra <mishravarun61@gmail.com>
2017-08-15 15:56:14 +0530

    new files

---
+++ resources/views/form/store.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.app')
+
+@section('content')
+
+<?php
+$url="/feedback/".$num;
+?>
+@if($num>=0)
+<div class="alert alert-success">
+	Your feedback form was successfully created
+</div>
+<div class="alert alert-primary">
+	Created by user <b>{{ $name }}</b> on <b>{{ $date }}</b>
+</div>
+<div class="alert alert-primary">
+	You can access your form <a href="{{ $url }}">here</a>
+</div>
+
+@else
+<div class="alert alert-danger">
+	This form was not created because you did not enter any fields!
+</div>
+@endif
+@endsection
